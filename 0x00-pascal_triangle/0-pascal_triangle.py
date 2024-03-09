5bb8c122a4a9d5c930517cf26d3721ddd24077d6
root <hirwawilly9@gmail.com>
2024-03-09 09:14:38 +0000

    Saving p-triangle file

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 
+
 # Defining the function
 def pascal_triangle(n):
     # Return / exit function if number given is less than 1
     if n <= 0:
         return []
     triangle = [[1]]
-    # Set a range of rows equal to n 
+    # Set a range of rows equal to n
     for i in range(1, n):
-        row = [1] # 1 always on first row
+        row = [1]  # 1 always on first row
         prev_row = triangle[i - 1]
-        # Creating rows
+        # Creating current rows numbers
         for j in range(1, i):
             row.append(prev_row[j - 1] + prev_row[j])
-        row.append(1) # 1 always on last row
+        row.append(1)  # 1 always on last row
         triangle.append(row)
     return triangle
